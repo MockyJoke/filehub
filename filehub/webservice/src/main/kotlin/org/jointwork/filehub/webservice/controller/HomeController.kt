9b93ee98b3670f1cb37d4ae24b52e2a0e04f5ace
Johnson <panjunsheng@msn.com>
2020-08-23 02:59:51 -0700

    Enable error displaying for debug

--- filehub/webservice/src/main/kotlin/org/jointwork/filehub/webservice/controller/HomeController.kt
+++ filehub/webservice/src/main/kotlin/org/jointwork/filehub/webservice/controller/HomeController.kt
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/") // Default route, optional
 class HomeController {
 
     @GetMapping("/")
     fun home(model: Model): String {
         Sample().add("head","tail")
         model["title"] = "Home"
-        return "Home"
+        return "home"
     }
 }
